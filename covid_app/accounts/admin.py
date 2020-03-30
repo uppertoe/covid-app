bdd221199318e30870f5e46637a46b062b4c096d
uppertoe <uppertoe@gmail.com>
2020-03-30 23:31:20 +1100

    Login using email address

--- covid_app/accounts/admin.py
+++ covid_app/accounts/admin.py
@@ -9,13 +9,31 @@ class UserProfileInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'user profile'
 
-# Define a new User admin
+'''
+Ensure that the email address is used as the username
+'''
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    inlines = (UserProfileInline,)
+    """Define admin model for custom User model with no email field."""
 
-# Re-register UserAdmin
-# admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    inlines = (UserProfileInline,)
 
 # Register your models here.
 admin.site.register(Availability)
--- covid_app/accounts/models.py
+++ covid_app/accounts/models.py
@@ -1,6 +1,6 @@
 import datetime
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -33,9 +33,52 @@ class State(models.Model):
     def __str__(self):
         return self.state_name
 
+'''
+Set up the user its attached profile
+'''
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('Please enter an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
-    #Included to allow extension later
-    pass
+    #Override default User class to allow login with email address
+    username = None
+    email = models.EmailField('email address', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -43,7 +86,7 @@ class UserProfile(models.Model):
     specialty = models.ManyToManyField(Specialty)
 
     def __str__(self):
-        return self.user.username
+        return self.user.email
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
